d90444b9e1a52eaf5c1c70d1f9fff968d96d9da5
Eden <edenyefet@gmail.com>
2021-01-15 23:04:23 +0200

    .

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,26 @@ mod database_logic;
 
 use actix_cors::Cors;
 use actix_web::{get, post, put, App, HttpResponse, HttpServer, Responder, web::Bytes};
-use actix_web::web::Query;
+use actix_web::web::{Query, Json};
 use serde::{Deserialize};
 
 extern crate simple_error;
 
+#[derive(Deserialize)]
+struct CreateRoomInfo {
+    title: String,
+}
+
 #[post("/create")]
-async fn create(bytes: Bytes) -> impl Responder {
-    let info: CreateRoomInfo = serde_json::from_str(&String::from_utf8(bytes.to_vec()).unwrap()).unwrap();
-    let response_body = logic::create_room(&info.title);
-    HttpResponse::Ok().header("Access-Control-Allow-Origin", "*").body(response_body)
+async fn create(create_room_info: Json<CreateRoomInfo>) -> impl Responder {
+    match logic::create_room(&create_room_info.title) {
+        Ok(response_body) => HttpResponse::Ok()
+            .header("Access-Control-Allow-Origin", "*")
+            .json(response_body),
+        Err(_) => HttpResponse::InternalServerError()
+            .header("Access-Control-Allow-Origin", "*")
+            .finish()
+    }
 }
 
 #[derive(Deserialize)]
@@ -20,14 +30,10 @@ struct ManageRoomQuery {
     room_uuid: String,
 }
 
-#[derive(Deserialize)]
-struct CreateRoomInfo {
-    title: String,
-}
 
 #[get("/manage")]
 async fn manage(manage_room_query: Query<ManageRoomQuery>) -> impl Responder {
-    match database_logic::get_room_info(manage_room_query.room_uuid.clone()) {
+    match database_logic::get_game_info(manage_room_query.room_uuid.clone()) {
         Ok(room_info) => HttpResponse::Ok().header("Access-Control-Allow-Origin", "*").body(room_info),
         Err(_) =>
             HttpResponse::NotFound().header("Access-Control-Allow-Origin", "*").finish()
@@ -36,7 +42,7 @@ async fn manage(manage_room_query: Query<ManageRoomQuery>) -> impl Responder {
 
 #[put("/save")]
 async fn save(bytes: Bytes, room_uuid: Query<ManageRoomQuery>) -> impl Responder {
-    match database_logic::update_room(room_uuid.room_uuid.clone(), String::from_utf8(bytes.to_vec()).unwrap()) {
+    match database_logic::update_game(room_uuid.room_uuid.clone(), String::from_utf8(bytes.to_vec()).unwrap()) {
         Ok(_) => HttpResponse::Ok().header("Access-Control-Allow-Origin", "*").finish(),
         Err(_) =>
             HttpResponse::InternalServerError().header("Access-Control-Allow-Origin", "*").finish()
