1ba25f01a7bd76b921d698235f673c313671f554
Barak <barak1224@gmail.com>
2021-01-18 23:35:00 +0200

    Renaming room to game

--- src/logic.rs
+++ src/logic.rs
@@ -2,6 +2,6 @@ use crate::database_logic;
 use crate::database_logic::GameInfo;
 use std::error::Error;
 
-pub fn create_room(room_name: &String) -> Result<GameInfo, Box<dyn Error>> {
+pub fn create_game(room_name: &String) -> Result<GameInfo, Box<dyn Error>> {
     database_logic::create_game(room_name)
 }
