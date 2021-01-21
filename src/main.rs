2b17d710e75574b4fe5f43bab7e7e514db2ba525
Eden <edenyefet@gmail.com>
2021-01-21 21:24:16 +0200

    Add logs and default header middlewares.

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,13 @@ mod websocket;
 
 use crate::data_model::GameInfo;
 use actix_cors::Cors;
+use actix_web::middleware::Logger;
 use actix_web::web::{Json, Query};
 use actix_web::{
-    get, post, put, web, App, Error, HttpRequest, HttpResponse, HttpServer, Responder,
+    get, middleware, post, put, web, App, Error, HttpRequest, HttpResponse, HttpServer, Responder,
 };
 use actix_web_actors::ws;
+use env_logger::Env;
 use serde::Deserialize;
 use uuid::Uuid;
 
@@ -30,12 +32,8 @@ struct CreateGameInfo {
 #[post("/create")]
 async fn create(create_game_info: Json<CreateGameInfo>) -> impl Responder {
     match logic::create_game(&create_game_info.title) {
-        Ok(response_body) => HttpResponse::Ok()
-            .header("Access-Control-Allow-Origin", "*")
-            .json(response_body),
-        Err(_) => HttpResponse::InternalServerError()
-            .header("Access-Control-Allow-Origin", "*")
-            .finish(),
+        Ok(response_body) => HttpResponse::Ok().json(response_body),
+        Err(_) => HttpResponse::InternalServerError().finish(),
     }
 }
 
@@ -48,50 +46,42 @@ struct ManageGameQuery {
 #[get("/manage")]
 async fn manage(manage_game_query: Query<ManageGameQuery>) -> impl Responder {
     match logic::get_game_info(manage_game_query.game_id) {
-        Ok(game_info) => HttpResponse::Ok()
-            .header("Access-Control-Allow-Origin", "*")
-            .json(game_info),
-        Err(_) => HttpResponse::NotFound()
-            .header("Access-Control-Allow-Origin", "*")
-            .finish(),
+        Ok(game_info) => HttpResponse::Ok().json(game_info),
+        Err(_) => HttpResponse::NotFound().finish(),
     }
 }
 
 #[put("/save")]
 async fn save(game_info: Json<GameInfo>) -> impl Responder {
     match logic::update_game(&game_info.0) {
-        Ok(_) => HttpResponse::Ok()
-            .header("Access-Control-Allow-Origin", "*")
-            .finish(),
-        Err(_) => HttpResponse::InternalServerError()
-            .header("Access-Control-Allow-Origin", "*")
-            .finish(),
+        Ok(_) => HttpResponse::Ok().finish(),
+        Err(_) => HttpResponse::InternalServerError().finish(),
     }
 }
 
 #[post("/start")]
 async fn start(game_id: Json<Uuid>) -> impl Responder {
     match logic::create_room(&game_id.0) {
-        Ok(room_info) => HttpResponse::Ok()
-            .header("Access-Control-Allow-Origin", "*")
-            .json(room_info),
-        Err(_) => HttpResponse::InternalServerError()
-            .header("Access-Control-Allow-Origin", "*")
-            .finish(),
+        Ok(room_info) => HttpResponse::Ok().json(room_info),
+        Err(_) => HttpResponse::InternalServerError().finish(),
     }
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let address = "0.0.0.0:9631";
-    let server = HttpServer::new(|| {
-        let cors = Cors::default()
-            .allow_any_origin()
-            .allow_any_method()
-            .allow_any_header();
 
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+    HttpServer::new(|| {
         App::new()
-            .wrap(cors)
+            .wrap(Logger::default())
+            .wrap(
+                Cors::default()
+                    .allow_any_origin()
+                    .allow_any_method()
+                    .allow_any_header(),
+            )
+            .wrap(middleware::DefaultHeaders::new().header("Access-Control-Allow-Origin", "*"))
             .service(create)
             .service(manage)
             .service(save)
@@ -99,11 +89,6 @@ async fn main() -> std::io::Result<()> {
             .service(web::resource("/ws/").route(web::get().to(ws_index)))
     })
     .bind(address)?
-    .run();
-    println!(
-        "Group trivia server has started and listening to {}",
-        address
-    );
-
-    server.await
+    .run()
+    .await
 }
