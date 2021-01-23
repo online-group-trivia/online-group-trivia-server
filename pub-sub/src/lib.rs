6cf2662e1f889dcf69118b55515b5f45d6e7ff90
dantepli <dan932@gmail.com>
2021-01-23 17:46:28 +0200

    initial commit on pub sub, blocking the thread

--- pub-sub/src/lib.rs
+++ pub-sub/src/lib.rs
@@ -1,1 +1,17 @@
+use redis;
+
+pub fn subscribe_to_topic(topic_name: &str) {
+    // TODO: connection pool, Use fred crate maybe
+    let client = redis::Client::open("redis://127.0.0.1/").unwrap();
+    let mut con = client.get_connection().unwrap();
+    let mut pubsub = con.as_pubsub();
+    pubsub.subscribe(topic_name).unwrap();
+
+    // TODO: Event loop
+    loop {
+        let msg = pubsub.get_message().unwrap();
+        let payload : String = msg.get_payload().unwrap();
+        println!("channel '{}': {}", msg.get_channel_name(), payload);
+    }
+}
 
--- websocket/src/websocket_routing.rs
+++ websocket/src/websocket_routing.rs
@@ -1,13 +1,21 @@
 use serde::Deserialize;
+use uuid::Uuid;
 
 #[derive(Deserialize)]
 enum WebsocketMessage {
     Echo { body: String },
+    ParticipantSubscription { room_id: String },
+    ManagerSubscription { game_id: Uuid, room_id: String },
 }
 
 pub fn handle_client_message(text: &str) -> String {
     let websocket_message: WebsocketMessage = serde_json::from_str(text).unwrap();
     match websocket_message {
         WebsocketMessage::Echo { body } => body,
+        WebsocketMessage::ParticipantSubscription { .. } => {}
+        WebsocketMessage::ManagerSubscription { game_id, room_id } => {
+            pub_sub::subscribe_to_topic(game_id);
+            pub_sub::subscribe_to_topic(room_id)
+        }
     }
 }
